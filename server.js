b9920c9d1dd53688598ee77211d7a26ad376088b
Stephen Provine <stephpr@microsoft.com>
2017-10-18 12:43:10 -0700

    Comment out redis stuff

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ var os = require('os');
 var express = require('express');
 var request = require('request');
 var contextful = require('./contextful');
-var redis = require('redis').createClient(process.env.REDIS_PORT, process.env.REDIS_HOST, {
-    auth_pass: process.env.REDIS_KEY,
-    tls: {
-        servername: process.env.REDIS_HOST
-    }
-});
+// var redis = require('redis').createClient(process.env.REDIS_PORT, process.env.REDIS_HOST, {
+//     auth_pass: process.env.REDIS_KEY,
+//     tls: {
+//         servername: process.env.REDIS_HOST
+//     }
+// });
 
 var app = express();
 app.use(express.static(__dirname + '/public'));
@@ -17,20 +17,20 @@ app.get('/', function (req, res) {
 });
 
 app.get('/api', function (req, res) {
-    redis.incr('requestCount');
+    // redis.incr('requestCount');
     request({
         uri: 'http://service-b',
         headers: contextful.from(req)
     }, function (error, response, body) {
-        res.send('Hello from service A running on ' + os.hostname() + ' and ' + body);
+        res.send('Hello from John\'s service A container ' + os.hostname() + ' and ' + body);
     });
 });
 
-app.get('/metrics', function (req, res) {
-    redis.get('requestCount', function (err, reply) {
-        res.send({ requestCount: reply });
-    });
-});
+// app.get('/metrics', function (req, res) {
+//     redis.get('requestCount', function (err, reply) {
+//         res.send({ requestCount: reply });
+//     });
+// });
 
 var port = 80;
 var server = app.listen(port, function () {
