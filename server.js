bb17136ae02daaa77e2d6f68a3c5a8b8af7053d3
Stephen Provine <stephpr@microsoft.com>
2019-03-08 13:01:25 -0800

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get('/metrics', function (req, res) {
     }
 });
 
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 3000;
 var server = app.listen(port, function () {
     console.log('Listening on port ' + port);
 });
